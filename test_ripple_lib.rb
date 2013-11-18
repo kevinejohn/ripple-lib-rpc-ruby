8e30157053031fc56574e2185ae3cfc7cbfd61f4
Kevin Johnson <kevinejohn@gmail.com>
2013-11-18 11:59:07 -0800

    Improved documentation

--- test_ripple_lib.rb
+++ test_ripple_lib.rb
@@ -6,21 +6,36 @@ ripple = Ripple.client({
   client_secret: "ssm5HPoeEZYJWvkJvQW9ro6e6hW9m"
 })
 
+# Send and verify with error checking
+success = false
 begin
-    success = true
+    failed = false
     begin
         puts "Sending transaction"
         tx_hash = ripple.send_currency("rfGKu3tSxwMFZ5mQ6bUcxWrxahACxABqKc", "USD", "0.0001")
+        success = true
     rescue Ripple::SubmitFailed
         # Handle failed submit
         puts "Transaction failed"
+        failed = true
     rescue Ripple::ServerUnavailable
         puts "Server Unavailable"
-        success = false
     end
-end while success == false
-# Verify transaction
-begin
-    puts "Checking transaction status"
-end while not ripple.transaction_suceeded?(tx_hash)
-puts "Transaction complete"
+end while not success and not failed
+if success
+    # Verify transaction
+    complete = false
+    begin
+      begin
+        puts "Checking transaction status"
+        complete = ripple.transaction_suceeded?(tx_hash)
+      rescue Ripple::ServerUnavailable
+        puts "Server Unavailable"
+      end
+      if not complete
+        # Sleep for small amount of time before checking again
+        sleep 0.5
+      end
+    end while not complete
+    puts "Transaction complete"
+end
